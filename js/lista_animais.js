a04aeb3f57267df97f8145b12c21e488aec8d8e2
aridsm <aniemorelanie@gmail.com>
2022-07-15 10:23:01 -0300

    redesign

---
+++ js/lista_animais.js
@@ -0,0 +1,70 @@
+const animais = [
+  {
+    nome: 'Pluto',
+    raca: 'SRD',
+    descricao: 'Donec dictum, purus ut fermentum aliquam, augue felis tempor neque.',
+    idade: '2 anos',
+    gen: 'Macho',
+    img: 'anima1.webp'
+  },
+  {
+    nome: 'Max',
+    raca: 'Pitbull Terrier',
+    descricao: ' Integer pellentesque nisl ligula, quis egestas risus bibendum nec.',
+    idade: '2 anos',
+    gen: 'Macho',
+    img: 'anima2.webp'
+  },
+  {
+    nome: 'Polly',
+    raca: 'SRD',
+    descricao: 'Nam malesuada ullamcorper ipsum quis pretium.',
+    idade: '3 meses',
+    gen: 'Fêmea',
+    img: 'anima3.webp'
+  },
+  {
+    nome: 'Lili',
+    raca: 'SRD',
+    descricao: 'Etiam sit amet nunc sed mauris laoreet molestie in id eros.',
+    idade: '1 ano',
+    gen: 'Fêmea',
+    img: 'anima4.webp'
+  },
+  {
+    nome: 'Zayn',
+    raca: 'SRD',
+    descricao: 'Quisque egestas vulputate lectus sit amet ultricies.',
+    idade: '3 meses',
+    gen: 'Macho',
+    img: 'anima5.webp'
+  },
+  {
+    nome: 'Millie',
+    raca: 'SRD',
+    descricao: 'Etiam sit amet nunc sed mauris laoreet molestie in id eros.',
+    idade: '4 meses',
+    gen: 'Fêmea',
+    img: 'anima6.webp'
+  },
+]
+
+export default animais
+
+/*
+<a class="item-animais" href="#">
+                                <p class="nome-animal">Pluto</p>
+                                <img src="img/anima1.webp" alt="Pluto">
+                                <div class="descricao-animais">
+                                    <span>
+                                        <p class="raca">SRD</p>
+                                        <p>Macho</p>
+                                    </span>
+                                    <p class="descricao">Etiam sit amet nunc sed mauris laoreet molestie in id eros.</p>
+                                    <span>
+                                        <p>Idade</p>
+                                        <p>2 anos</p>
+                                    </span>
+                                </div>
+                            </a>
+                            */
---
+++ js/script.js
@@ -0,0 +1,148 @@
+import animais from './lista_animais.js';
+
+function menu() {
+  const menuMobile = document.querySelector('.js-menu-mobile');
+  const navMobile = document.querySelector('.js-nav-mobile')
+
+  menuMobile.addEventListener('click', function () {
+    navMobile.classList.toggle('ativo');
+    menuMobile.classList.toggle('ativo');
+  })
+}
+menu()
+
+
+function loadCardsAnimais() {
+  const containerAnimais = document.querySelector('.js-scroll-animais');
+  animais.forEach(animal => createCardAnimal(animal))
+
+  function createCardAnimal(animal) {
+    const diretorio = './img/';
+    const newCard = document.createElement('div');
+    newCard.classList.add('card')
+    const cardContent = `
+    <div class="borda-card-animais">
+        <a class="item-animais" href="#">
+            <p class="nome-animal">${animal.nome}</p>
+            <div class="img"><img src="${diretorio + animal.img}" alt="${animal.nome}"></div>
+            <div class="descricao-animais">
+                <span>
+                    <p class="raca">${animal.raca}</p>
+                    <p>${animal.gen}</p>
+                </span>
+                <p class="descricao">${animal.descricao}</p>
+                <span>
+                    <p>Idade</p>
+                    <p>${animal.idade}</p>
+                </span>
+            </div>
+        </a>
+    </div>
+  `;
+    newCard.innerHTML = cardContent
+    containerAnimais.appendChild(newCard)
+  }
+
+}
+loadCardsAnimais()
+
+function slider() {
+  const btnAnterior = document.querySelector('.js-btn-anterior');
+  const btnProximo = document.querySelector('.js-btn-proximo');
+  const containerAnimais = document.querySelector('.js-scroll-animais');
+  let slidesPerPage = 4;
+  let totalSlideItems = containerAnimais.children.length;
+  let itemWidth = containerAnimais.firstElementChild.offsetWidth;
+  let gapBetweenItems = +getComputedStyle(containerAnimais)['grid-column-gap'].replace('%', '');
+  const slideWidth = itemWidth + containerAnimais.offsetWidth * gapBetweenItems / 100;
+  let currentSlide = 0;
+
+  function setPositionSlide() {
+    containerAnimais.style.transform = `translateX(-${slideWidth * currentSlide + 'px'})`;
+  }
+
+  function previousSlide() {
+    if (currentSlide == 0) {
+      return;
+    }
+    --currentSlide;
+    setPositionSlide();
+  }
+
+  function nextSlide() {
+    if (currentSlide + slidesPerPage == totalSlideItems) {
+      return;
+    }
+    ++currentSlide;
+    setPositionSlide();
+  }
+
+  function dragSlide(e) {
+    console.log(e.x)
+  }
+
+  btnAnterior.addEventListener('click', previousSlide)
+  btnProximo.addEventListener('click', nextSlide)
+  containerAnimais.addEventListener('dragstart', dragSlide)
+  containerAnimais.addEventListener('dragend', dragSlide)
+}
+
+slider()
+
+
+function scrollAnimation() {
+  const itemScroll = document.querySelectorAll('.js-scroll');
+  const windowTop = window.innerHeight * 0.7
+  function scrollAnima() {
+    itemScroll.forEach((item) => {
+      const topo = item.getBoundingClientRect().top - windowTop;
+      if (topo < 0) {
+        item.classList.add('anima-scroll')
+      }
+    })
+  }
+  scrollAnima()
+  window.addEventListener('scroll', scrollAnima)
+}
+scrollAnimation()
+
+
+function numeroScroll() {
+  const numeros = document.querySelectorAll('.js-numero')
+  const containerNumeros = document.querySelector('.numeros')
+  const windowTop = window.innerHeight * 0.7
+  const topoNumeros = containerNumeros.getBoundingClientRect().top - windowTop;
+
+  if (topoNumeros < 0) {
+    numeros.forEach(numero => {
+      const valor = +numero.innerText
+      const incremento = Math.floor(valor / 20);
+      let start = 0;
+      const timer = setInterval(() => {
+        start = start + incremento;
+        numero.innerText = start;
+        if (start > valor) {
+          numero.innerText = valor;
+          clearInterval(timer)
+        }
+      }, 50)
+    })
+    window.removeEventListener('scroll', numeroScroll)
+  }
+}
+window.addEventListener('scroll', numeroScroll)
+
+
+function updateHeaderScrolled() {
+  const header = document.querySelector('.js-header')
+  const pageScrolled = window.scrollY;
+
+  if (pageScrolled > 0) {
+    header.classList.add('scrolled')
+  } else {
+    header.classList.remove('scrolled')
+  }
+
+}
+
+window.addEventListener('scroll', updateHeaderScrolled)
